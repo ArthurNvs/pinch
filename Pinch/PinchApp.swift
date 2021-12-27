6aedb2150af592dcc3dc71ab37de979f22bcc9bb
Arthur Neves <arthurnvsm@gmail.com>
2021-12-27 09:04:01 -0300

    Project first setup

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Arthur Neves on 27/12/21.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
