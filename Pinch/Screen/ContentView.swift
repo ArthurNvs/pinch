c6623edf119787782a4258d000d811e4a2840300
Arthur Neves <arthurnvsm@gmail.com>
2021-12-27 09:22:51 -0300

    Image animation implementations

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State private var isAnimating = false
+  
   var body: some View {
     NavigationView {
       ZStack {
@@ -18,9 +20,15 @@ struct ContentView: View {
           .cornerRadius(10)
           .padding()
           .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+          .opacity(isAnimating ? 1 : 0)
       } //: ZStack
     .navigationTitle("Pinch & Zoom")
     .navigationBarTitleDisplayMode(.inline)
+    .onAppear(perform: {
+      withAnimation(.linear(duration: 1)) {
+        isAnimating = true
+      }
+    })
     } //: NavigationView
     .navigationViewStyle(.stack)
   }
