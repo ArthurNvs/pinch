c1b3c61f3a12b88e438f65a65608def1a1b57a25
Arthur Neves <arthurnvsm@gmail.com>
2021-12-27 09:17:52 -0300

    Image first layout

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -8,15 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+  var body: some View {
+    NavigationView {
+      ZStack {
+        // MARK: - PAGE IMAGE
+        Image("magazine-front-cover")
+          .resizable()
+          .aspectRatio(contentMode: .fit)
+          .cornerRadius(10)
+          .padding()
+          .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+      } //: ZStack
+    .navigationTitle("Pinch & Zoom")
+    .navigationBarTitleDisplayMode(.inline)
+    } //: NavigationView
+    .navigationViewStyle(.stack)
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-      ContentView()
-        .preferredColorScheme(.dark)
-    }
+  static var previews: some View {
+    ContentView()
+      .preferredColorScheme(.dark)
+  }
 }
