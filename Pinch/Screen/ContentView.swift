5e9cc3b1d17086824619278eefd61c3811a4b71a
Arthur Neves <arthurnvsm@gmail.com>
2021-12-27 18:08:46 -0300

    Image double tap gesture implementation

--- Pinch/Screen/ContentView.swift
+++ Pinch/Screen/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
   @State private var isAnimating = false
+  @State private var imageScale: CGFloat = 1
   
   var body: some View {
     NavigationView {
@@ -21,6 +22,19 @@ struct ContentView: View {
           .padding()
           .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
           .opacity(isAnimating ? 1 : 0)
+          .scaleEffect(imageScale)
+          // MARK: - 1. TAP GESTURE
+          .onTapGesture(count: 2) {
+            if imageScale == 1 {
+              withAnimation(.spring()) {
+                imageScale = 5
+              }
+            } else {
+              withAnimation(.spring()) {
+                imageScale = 1
+              }
+            }
+          }
       } //: ZStack
     .navigationTitle("Pinch & Zoom")
     .navigationBarTitleDisplayMode(.inline)
