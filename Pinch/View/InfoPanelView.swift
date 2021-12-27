558809d52a4a1fe5bb5a7a649d3faf8cf391ffd3
Arthur Neves <arthurnvsm@gmail.com>
2021-12-27 19:11:52 -0300

    Info Panel created

---
+++ Pinch/View/InfoPanelView.swift
@@ -0,0 +1,52 @@
+//
+//  InfoPanelView.swift
+//  Pinch
+//
+//  Created by Arthur Neves on 27/12/21.
+//
+
+import SwiftUI
+
+struct InfoPanelView: View {
+  var scale: CGFloat
+  var offset: CGSize
+  
+  var body: some View {
+    HStack {
+      // MARK: - HOTSPOT
+      Image(systemName: "circle.circle")
+        .symbolRenderingMode(.hierarchical)
+        .resizable()
+        .frame(width: 30, height: 30)
+      
+      Spacer()
+      
+      // MARK: - INFO PANEL
+      HStack(spacing: 2) {
+        Image(systemName: "arrow.up.left.and.arrow.down.right")
+        Text("\(scale)")
+        
+        Spacer()
+        
+        Image(systemName: "arrow.left.and.right")
+        Text("\(offset.width)")
+        
+        Spacer()
+        
+        Image(systemName: "arrow.up.and.down")
+        Text("\(offset.height)")
+        
+        Spacer()
+      }
+    }
+  }
+}
+
+struct InfoPanelView_Previews: PreviewProvider {
+  static var previews: some View {
+    InfoPanelView(scale: 1, offset: .zero)
+      .preferredColorScheme(.dark)
+      .previewLayout(.sizeThatFits)
+      .padding()
+  }
+}
